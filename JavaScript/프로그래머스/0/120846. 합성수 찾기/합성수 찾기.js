e6f395768c802a7de938aa4af90363c648b8eb8d
서영 <4tres1@gmail.com>
2026-01-21 17:40:48 +0900

    [level 0] Title: 합성수 찾기, Time: 0.07 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ JavaScript/프로그래머스/0/120846. 합성수 찾기/합성수 찾기.js
@@ -0,0 +1,14 @@
+function solution(n) {
+    let count = 0;
+    
+    for (let i=2; i<=n; i++) {
+        for (let j=2; j<i; j++) {
+            if (i % j == 0) {
+                count++;
+                break;
+            }
+        }
+    }
+    
+    return count;
+}
